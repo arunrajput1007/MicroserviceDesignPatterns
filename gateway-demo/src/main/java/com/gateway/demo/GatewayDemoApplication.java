70b652b179cd339d6b744be078adfbb2896fc537
arunrajput1007 <arunrajput1007@gmail.com>
2020-06-25 13:39:59 +0530

    added gateway

---
+++ gateway-demo/src/main/java/com/gateway/demo/GatewayDemoApplication.java
@@ -0,0 +1,24 @@
+package com.gateway.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class GatewayDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GatewayDemoApplication.class, args);
+	}
+
+	@Bean
+	public RouteLocator routingLogic(RouteLocatorBuilder builder){
+		return builder.routes()
+				.route(r1 -> r1.host("i.feel.lucky:8006")
+						.and().path("/headerrouting/**")
+						.uri("http://httpstat.us"))
+				.build();
+	}
+}
--- sd-demo/src/main/java/com/sd/demo/sddemo/SdDemoApplication.java
+++ sd-demo/src/main/java/com/sd/demo/sddemo/SdDemoApplication.java
@@ -11,5 +11,4 @@ public class SdDemoApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(SdDemoApplication.class, args);
 	}
-
 }
